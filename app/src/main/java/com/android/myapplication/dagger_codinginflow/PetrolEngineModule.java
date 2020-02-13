bb4c63aeac3df0b569d5973c15378dedd1b162a9
Hady Salhab <hadysalhab1@gmail.com>
2020-02-13 18:14:49 +1100

    method 2

--- app/src/main/java/com/android/myapplication/dagger_codinginflow/PetrolEngineModule.java
+++ app/src/main/java/com/android/myapplication/dagger_codinginflow/PetrolEngineModule.java
@@ -1,14 +1,13 @@
 package com.android.myapplication.dagger_codinginflow;
 
+import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 
 @Module
-public class PetrolEngineModule {
-    @Provides
+public abstract class PetrolEngineModule {
+    @Binds
     //we can directly pass the petrol engine here because constructor is already annotated with @Inject
-    Engine provideEngine(PetrolEngine engine) {
-        return engine;
-    }
+    abstract Engine bindEngine(PetrolEngine engine);
 
 }
