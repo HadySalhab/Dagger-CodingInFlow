51c1858085fba6df23a94b3feb168efa066ef88d
Hady Salhab <hadysalhab1@gmail.com>
2020-02-13 13:37:26 +1100

    feat: part2

---
+++ app/src/main/java/com/android/myapplication/dagger_codinginflow/CarComponent.java
@@ -0,0 +1,8 @@
+package com.android.myapplication.dagger_codinginflow;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+}
---
+++ app/src/main/java/com/android/myapplication/dagger_codinginflow/Wheels.java
@@ -0,0 +1,10 @@
+package com.android.myapplication.dagger_codinginflow;
+
+import javax.inject.Inject;
+
+public class Wheels {
+    @Inject
+    Wheels(){
+
+    }
+}
